7a5918bfe145c5f2cf85627fd679c6d6aa157985
Xiran Jia <jiaxiran98@gmail.com>
2024-05-16 22:11:46 -0700

    4.9 done
    Change-Id: I99a156f7d1f290316129cc8294c0f4225359c02f

---
+++ src/Kanbas/Courses/Modules/GreyBanmark.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { IoBanOutline } from "react-icons/io5";
+import { FaCircle } from "react-icons/fa";
+export default function GreenCheckmark() {
+  return (
+    <span className="me-1 position-relative">
+      <IoBanOutline className="text-tertiary me-1 mt-2 position-absolute fs-5" />
+      <FaCircle className="text-white me-1 fs-6" />
+    </span>
+  );
+}
